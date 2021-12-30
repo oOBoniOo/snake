e7c661044b444b08f9cff826474fb44ed385e03a
oOBoniOo <boni85@gmail.com>
2021-12-30 20:15:04 +0100

    revirtiendo cambios

--- src/actors/Feed.ts
+++ src/actors/Feed.ts
@@ -2,78 +2,86 @@ import { Objets } from "../state/Objets";
 import { Point } from "../types/Point";
 import { angleToRad } from "../utils/angleToRad";
 import { Actor, IActor } from "./Actor";
-import { Snake } from "./Snake";
 
 export class Feed extends Actor {
-  blockSize: number;
-  snake: Snake;
-  touched: boolean;
-  //barrierindex: number;
-  constructor(
-    initialPos: Point,
-    bw = 20,
-    snake: Snake
-    //barrierindex: number,
-  ) {
-    super(initialPos);
-    this.blockSize = bw;
-    // this.angle = angle;
-    this.snake = snake;
-    this.touched = false;
-    //this.barrierindex = barrierindex;
-  }
-  update() {
-    let snakePos = this.snake.position;
-    let myPos = this.position;
-    let xDistance = Math.abs(myPos.x - snakePos.x);
-    let yDistnace = Math.abs(myPos.y - snakePos.y);
+	feedWith: number;
+	snake: IActor;
+	touched: boolean;
+	//barrierindex: number;
+	constructor(
+		initialPos: Point,
+		bw = 25,
+		snake: IActor,
+		//barrierindex: number,
+	) {
+		super(initialPos);
+		this.feedWith = bw;
+		// this.angle = angle;
+		this.snake = snake;
+		this.touched = false;
+		//this.barrierindex = barrierindex;
+	}
+	update() {
+		let snakeAngle = this.snake.angle;
+		let snakePos = this.snake.position;
+		let myPos = this.position;
+		let xDistance = Math.abs(myPos.x - snakePos.x);
+		let yDistnace = Math.abs(myPos.y - snakePos.y);
 
-    if (this.position.x == snakePos.x && this.position.y == snakePos.y) {
-      console.log("TOCADO");
-      this.touched = true;
-      this.snake.updateLenght();
-    }
-    // console.log(Circuit.currentBarrier, this.barrierindex);
-    // if (snakeAngle == 90 || snakeAngle == 270) {
-    //   if (xDistance < Math.abs(30) && yDistnace < 2) {
-    //     this.touched = true;
-    //     console.log("OBJETO", this.position);
-    //     console.log("SNAKE", snakePos);
-    //     console.log("TOCADAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    //   }
-    // }
-    // if (snakeAngle == 0 || snakeAngle == 180) {
-    //   if (yDistnace < Math.abs(30) && xDistance < 2) {
-    //     this.touched = true;
-    //     console.log("OBJETO", this.position);
-    //     console.log("SNAKE", snakePos);
-    //     console.log("TOCADAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    //   }
-    // }
-  }
-  draw(delta: number, ctx: CanvasRenderingContext2D) {
-    // Trasladamos y toamos el canvas según la barrera que vayamos a pintar
+		// console.log(Circuit.currentBarrier, this.barrierindex);
+		if ((snakeAngle == 90) || (snakeAngle == 270)) {
+			if ((xDistance < Math.abs(30)) && (yDistnace < 2)) {
+				this.touched = true;
+				console.log('OBJETO',this.position)
+				console.log('SNAKE',snakePos)
+				console.log('TOCADAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+			}
+		}
+		if ((snakeAngle == 0) || (snakeAngle == 180)) {
+			if ((yDistnace < Math.abs(30)) && (xDistance < 2)) {
+				this.touched = true;
+				console.log('OBJETO',this.position)
+				console.log('SNAKE',snakePos)
+				console.log('TOCADAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+			}
+		}
+			
+	}
+	draw(delta: number, ctx: CanvasRenderingContext2D) {
+		// Trasladamos y toamos el canvas según la barrera que vayamos a pintar
 
-    // ctx.translate(
-    //   this.position.x * this.blockSize,
-    //   this.position.y * this.blockSize
-    // );
-    //ctx.rotate(angleToRad(this.angle));
-    // this.touched ? (ctx.strokeStyle = "green") : (ctx.strokeStyle = "red");
-    // Pintamos una línea
-    // ctx.lineWidth = 1;
-    // ctx.beginPath();
-    // ctx.moveTo(this.feedWith / 2, 0);
-    // //ctx.lineTo(this.feedWith / 2, 0);
-    // ctx.arc(0, 0, this.feedWith, 0, angleToRad(360));
+		ctx.translate(this.position.x, this.position.y);
+		//ctx.rotate(angleToRad(this.angle));
+		// this.touched ? (ctx.strokeStyle = "green") : (ctx.strokeStyle = "red");
+		// Pintamos una línea
+		// ctx.lineWidth = 1;
+		// ctx.beginPath();
+		// ctx.moveTo(this.feedWith / 2, 0);
+		// //ctx.lineTo(this.feedWith / 2, 0);
+		// ctx.arc(0, 0, this.feedWith, 0, angleToRad(360));
 
-    ctx.fillStyle = "#d31062";
-    ctx.lineWidth = 1;
-    ctx.fillRect(
-      (this.position.x - 1) * this.blockSize,
-      (this.position.y - 1) * this.blockSize,
-      this.blockSize,
-      this.blockSize
-    );
-  }
+		ctx.fillStyle = "#d31062";
+					ctx.lineWidth = 6;
+					var X = 0;
+					var Y = 0;
+					var R = 20;
+																	
+					var L = 9;
+					var paso = 2	
+																
+					var estrella= L / paso
+					var rad = (2*Math.PI) / estrella;
+																	
+					ctx.beginPath();
+							for( var i = 0; i < L; i++ ){
+							let x = X + R * Math.cos( rad*i );
+							let y = Y + R * Math.sin( rad*i );
+							ctx.lineTo(x, y);
+							}
+
+
+		ctx.closePath();
+		ctx.stroke();
+		ctx.fill();
+	}
 }
--- src/actors/Map.ts
+++ src/actors/Map.ts
@@ -1,43 +1,71 @@
-import { Actor, IActor } from "./Actor";
-import { Point } from "../types/Point";
-import { map } from "lodash";
+import { Actor } from "./Actor";
 
 const level = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
+	"WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+	"W............WW............W",
+	"W.WWWW.WWWWW.WW.WWWWW.WWWW.W",
+	"W*WWWW.WWWWW.WW.WWWWW.WWWW*W",
+	"W.WWWW.WWWWW.WW.WWWWW.WWWW.W",
+	"W..........................W",
+	"W.WWWW.WW.WWWWWWWW.WW.WWWW.W",
+	"W.WWWW.WW.WWWWWWWW.WW.WWWW.W",
+	"W......WW....WW....WW......W",
+	"WWWWWW.WWWWW.WW.WWWWW.WWWWWW",
+	"WWWWWW.WWWWW.WW.WWWWW.WWWWWW",
+	"WWWWWW.WW..........WW.WWWWWW",
+	"WWWWWW.WW.WWW--WWW.WW.WWWWWW",
+	"WWWWWW.WW.WooooooW.WW.WWWWWW",
+	"..........WooooooW..........",
+	"WWWWWW.WW.WooooooW.WW.WWWWWW",
+	"WWWWWW.WW.WWWWWWWW.WW.WWWWWW",
+	"WWWWWW.WW..........WW.WWWWWW",
+	"WWWWWW.WW.WWWWWWWW.WW.WWWWWW",
+	"WWWWWW.WW.WWWWWWWW.WW.WWWWWW",
+	"W............WW............W",
+	"W.WWWW.WWWWW.WW.WWWWW.WWWW.W",
+	"W*WWWW.WWWWW.WW.WWWWW.WWWW*W",
+	"W...WW................WW...W",
+	"WWW.WW.WW.WWWWWWWW.WW.WW.WWW",
+	"WWW.WW.WW.WWWWWWWW.WW.WW.WWW",
+	"W......WW....WW....WW......W",
+	"W.WWWWWWWWWW.WW.WWWWWWWWWW.W",
+	"W.WWWWWWWWWW.WW.WWWWWWWWWW.W",
+	"W..........................W",
+	"WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+].map((f) => f.split(""));
 
-export class Map {
-	update(coord: Point, value: number) {
-		coord.x = (coord.x >= 0) ? ((coord.x <=map.length-1) ?  24|coord.x)|  0
-		
-		if ((coord.x >= 0) && (coord.y >= 0)) { 
+export class Map extends Actor {
+	update() {}
+	keyboard_event() {}
+	draw(delta: number, ctx: CanvasRenderingContext2D) {
+		let total_ratio = 1024 / level.length;
+		let spacing = total_ratio;
+		let size = total_ratio;
+		ctx.save();
+		ctx.translate(50, 0);
+		for (let x = 0; x < level.length; x++) {
+			for (let y = 0; y < level[x].length; y++) {
+				ctx.beginPath();
 
+				if (level[x][y] == "W") {
+					ctx.rect(y * spacing, x * spacing, size, size);
+				}
+
+				if (level[x][y] == ".") {
+					ctx.arc(
+						y * spacing + size / 2,
+						x * spacing + size / 2,
+						5,
+						0,
+						2 * Math.PI,
+					);
+				}
+
+				ctx.closePath();
+				ctx.fill();
+				ctx.stroke();
+			}
 		}
+		ctx.restore();
 	}
-  keyboard_event() {}
-  draw(delta: number, ctx: CanvasRenderingContext2D) {}
 }
